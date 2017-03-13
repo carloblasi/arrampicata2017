64ea428908c75a042ff988d7cb440902613c1c7f
Aurelio Pennacchio <Aurelio@iMac-di-Aurelio.homenet.telecomitalia.it>
2017-03-13 21:05:07 +0100

    Form per selezione elenchi degli atleti

--- application/view/admin/genera_elenchi.php
+++ application/view/admin/genera_elenchi.php
@@ -1,5 +1,46 @@
 	<div class="contenitore2">
-		<h1>Elenco di tutti gli atleti maschi</h1>
+		
+		<form class="pure-form pure-form-aligned" action="<?php echo URL; ?>/admin/generaElenchi" method="POST">
+			<fieldset>
+				<div class="pure-control-group">
+					<label for="elenco">Elenco</label>
+					<select name="elenco">
+						<option value="m-giovanissimi">Maschi giovanissimi</option>
+						<option value="m-juniores">Maschi juniores</option>
+						<option value="f-giovanissime">Femmine giovanissime</option>
+						<option value="f-juniores">Femmine juniores</option>
+					</select>
+				</div>
+				<div class="pure-controls">
+					<button type="submit" class="pure-button pure-button-primary" name="g-elenco">Genera elenco</button>
+				</div>
+			</fieldset>
+		</form>
+		<?php 
+			$modo = 'm-giovanissimi';
+			if(isset($_POST['elenco'])){
+				$modo = $_POST['elenco'];
+			}
+			$titolo;
+			switch ($modo) {
+				case 'm-giovanissimi':
+					$titolo = 'Maschi giovanissimi';
+					break;
+				case 'm-juniores':
+			 		$titolo = 'Maschi juniores';
+				 		break;
+			 	case 'f-giovanissime':
+					$titolo = 'Femmine giovanissime';
+					break;
+			 	case 'f-juniores':
+					$titolo = 'Femmine juniores';
+					break;
+				default:
+					$liste = 'Maschi giovanissimi';
+						break;
+			}
+		?>
+		<h1><?php echo $titolo; ?></h1>
 		<table class="pure-table">
 			<thead>
 				<tr>
@@ -10,7 +51,27 @@
 				</tr>
 			</thead>
 			<tbody>
-				<?php $liste = $this->model->getAllAtletiMaschi(); ?>
+				<?php
+					$liste;
+					switch ($modo) {
+					 	case 'm-giovanissimi':
+					 		$liste = $this->model->getAllAtletiMaschiAllievi();
+					 		break;
+					 	case 'm-juniores':
+					 		$liste = $this->model->getAllAtletiMaschiJuniores();
+					 		break;
+					 	case 'f-giovanissime':
+					 		$liste = $this->model->getAllAtletiFemmineAllievi();
+					 		break;
+					 	case 'f-juniores':
+					 		$liste = $this->model->getAllAtletiFemmineJuniores();
+					 		break;
+					 	default:
+					 		$liste = $this->model->getAllAtletiMaschi();
+					 		break;
+					} 
+					 
+				?>
 				<?php foreach ($liste as $lista): ?>
 				<tr>
 					<td><?php echo $lista->nome; ?></td>
@@ -34,98 +95,6 @@
 			</thead>
 			<tbody>
 				<?php $liste = $this->model->getAllAtletiFemmine(); ?>
-				<?php foreach ($liste as $lista): ?>
-				<tr>
-					<td><?php echo $lista->nome; ?></td>
-					<td><?php echo $lista->cognome; ?></td>
-					<td><?php echo $lista->data_nascita; ?></td>
-					<td><?php echo $lista->nome_scuola; ?></td>
-				</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
-
-		<h1>Elenco di tutti gli atleti maschi giovanissmi</h1>
-		<table class="pure-table">
-			<thead>
-				<tr>
-					<th>Nome</th>
-					<th>Cognome</th>
-					<th>Data di nascita</th>
-					<th>Scuola</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php $liste = $this->model->getAllAtletiMaschiAllievi(); ?>
-				<?php foreach ($liste as $lista): ?>
-				<tr>
-					<td><?php echo $lista->nome; ?></td>
-					<td><?php echo $lista->cognome; ?></td>
-					<td><?php echo $lista->data_nascita; ?></td>
-					<td><?php echo $lista->nome_scuola; ?></td>
-				</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
-
-		<h1>Elenco di tutte le atlete femmine giovanissimi</h1>
-		<table class="pure-table">
-			<thead>
-				<tr>
-					<th>Nome</th>
-					<th>Cognome</th>
-					<th>Data di nascita</th>
-					<th>Scuola</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php $liste = $this->model->getAllAtletiFemmineAllievi(); ?>
-				<?php foreach ($liste as $lista): ?>
-				<tr>
-					<td><?php echo $lista->nome; ?></td>
-					<td><?php echo $lista->cognome; ?></td>
-					<td><?php echo $lista->data_nascita; ?></td>
-					<td><?php echo $lista->nome_scuola; ?></td>
-				</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
-
-		<h1>Elenco di tutti gli atleti maschi juniores</h1>
-		<table class="pure-table">
-			<thead>
-				<tr>
-					<th>Nome</th>
-					<th>Cognome</th>
-					<th>Data di nascita</th>
-					<th>Scuola</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php $liste = $this->model->getAllAtletiMaschiJuniores(); ?>
-				<?php foreach ($liste as $lista): ?>
-				<tr>
-					<td><?php echo $lista->nome; ?></td>
-					<td><?php echo $lista->cognome; ?></td>
-					<td><?php echo $lista->data_nascita; ?></td>
-					<td><?php echo $lista->nome_scuola; ?></td>
-				</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
-
-		<h1>Elenco di tutte le atlete femmine juniores</h1>
-		<table class="pure-table">
-			<thead>
-				<tr>
-					<th>Nome</th>
-					<th>Cognome</th>
-					<th>Data di nascita</th>
-					<th>Scuola</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php $liste = $this->model->getAllAtletiFemmineJuniores(); ?>
 				<?php foreach ($liste as $lista): ?>
 				<tr>
 					<td><?php echo $lista->nome; ?></td>
