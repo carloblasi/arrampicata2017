15ab2740e1e71360066547b71a20cad9f6bb3090
drTerrible <gabriele.rox97@gmail.com>
2017-03-15 20:53:26 +0100

    piccola aggiunta

--- application/view/admin/classifica.php
+++ application/view/admin/classifica.php
@@ -6,32 +6,35 @@
 	<link rel="stylesheet" href="<?php echo URL; ?>css/pure-min.css">
 	<link rel="stylesheet" href="<?php echo URL; ?>css/grids-responsive-min.css">
 	<link rel="stylesheet" href="<?php echo URL; ?>css/font-awesome/css/font-awesome.min.css" type="text/css">
-	<link rel="stylesheet" href="<?php echo URL; ?>css/font-awesome/css/classifica.css" type="text/css">
+	<link rel="stylesheet" href="<?php echo URL; ?>css/classifica.css" type="text/css">
 	<title>Classifica</title>
 </head>
-<body onload="">
-	<div>
+<body>
+	<div class="center">
 		<div class="pure-g">
-			<div class="pure-u-1-2" id="classifica1"> </div>
-			<div class="pure-u-1-2" id="classifica2"> </div>
+			<div class="pure-u-1-2" id="table1"></div>
+			<div class="pure-u-1-2" id="table2"></div>
+		</div>
+		<div class="pure-g">
+			<div class="pure-u-1-2" id="table3"></div>
+			<div class="pure-u-1-2" id="table4"></div>
 		</div>
 	</div>
 	<script>
-
 		function refreshData()
 		{
 	  	// Load the content of "path/to/script.php" into an element with ID "#container".
-	  		$('#classifica1').load('generaClassifica/giovanissimiM');
-			$('#classifica2').load('generaClassifica/giovanissimiM');
+	  	$('#table1').load('generaClassifica/giovanissimiM');
+			$('#table2').load('generaClassifica/giovanissimiM');
+			$('#table3').load('generaClassifica/giovanissimiM');
+			$('#table4').load('generaClassifica/giovanissimiM');
 		}
 
 		$(document).ready(function(){
 			refreshData();
 		});
-
 		// Execute every 5 seconds
-		window.setInterval(refreshData, 1000);
-
+		window.setInterval(refreshData, 10000);
 	</script>
 </body>
 </html>
