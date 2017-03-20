4665683be95f37526d4375b74972bf50e2eb1e5c
Carlo Blasi <carloblasi98@gmail.com>
2017-03-20 21:52:33 +0100

    Cambiando il menu admin
    Non definitivo

--- application/view/admin/menu.php
+++ application/view/admin/menu.php
@@ -9,29 +9,38 @@
 	<link rel="stylesheet" href="<?php echo URL; ?>css/font-awesome/css/font-awesome.min.css" type="text/css">
 	<style type="text/css">
 
-		.contenitore {
-			
-			text-align: center;
-			background-color: rgb(66,184,221);
-		}
-
 		body {
 			background-color: #F6F1ED;
+		}
+
+		.pure-menu {
+			
+			text-align: center;
+			background-color: #F6F1ED;
+			padding: 6px;
+		}
+
+		.pure-menu-item {
+			margin-right: 2px;
+			margin-left: 2px;
 		}
 
 		.contenitore1, .contenitore-ultimo {
 
 			margin-left: 30%;
 			margin-right: 30%;
-			margin-top: 10%
+			margin-top: 10%;
 		}
 
-		.pure-menu-link {
-			color: white;
-		}
+		.pure-button {
 
-		.pure-menu-link:hover {
-			color: #666;
+			color: #F6F1ED;
+			border-radius: 6px;
+			text-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+			background: rgb(66, 184, 221);
+			font-size: 110%;
+			font-weight: 800;
+			text-align: center;
 		}
 
 		.contenitore2 {
@@ -42,6 +51,7 @@
 			margin-top: 2%;
 			margin-bottom: 1%;
 		}
+
 		tr, td {
 			text-align: center;
 		}
@@ -49,13 +59,22 @@
 	</style>
 </head>
 <body>
-	<div class="pure-menu pure-menu-horizontal contenitore">
-		<a href="<?php echo URL; ?>admin/aggiungistudente" class="pure-menu-heading pure-menu-link">Admin</a>
+	<div class="pure-menu pure-menu-horizontal">
 		<ul class="pure-menu-list">
-			<li class="pure-menu-item"><a href="<?php echo URL; ?>admin/aggiungistudente" class="pure-menu-link">Aggiungi</a></li>
-			<li class="pure-menu-item"><a href="<?php echo URL; ?>admin/classifica" class="pure-menu-link">Classifica</a></li>
-			<li class="pure-menu-item"><a href="<?php echo URL; ?>admin/generaPettorine" class="pure-menu-link">Pettorine</a></li>
-			<li class="pure-menu-item"><a href="<?php echo URL; ?>admin/elenco" class="pure-menu-link">Elenchi</a></li>
-			<li class="pure-menu-item"><a href="<?php echo URL; ?>admin/correzione" class="pure-menu-link">Correzione</a></li>
+			<li class="pure-menu-item">
+				<a href="<?php echo URL; ?>admin/aggiungistudente" class="pure-button">Aggiungi Studente</a>
+			</li>
+			<li class="pure-menu-item">
+				<a href="<?php echo URL; ?>admin/classifica" class="pure-button">Classifica</a>
+			</li>
+			<li class="pure-menu-item">
+				<a href="<?php echo URL; ?>admin/generaPettorine" class="pure-button">Pettorine</a>
+			</li>
+			<li class="pure-menu-item">
+				<a href="<?php echo URL; ?>admin/elenco" class="pure-button ">Elenchi</a>
+			</li>
+			<li class="pure-menu-item">
+				<a href="<?php echo URL; ?>admin/correzione" class="pure-button">Correzione</a>
+			</li>
 		</ul>
 	</div>
